eb14fab35c8265117b302d8fdc88df51b17559ce
Isobel Beasley <ibeasley@student.unimelb.edu.au>
2024-11-30 22:58:10 -0800

    Fix download link for gwas summary stats

--- code/download_gwas.sh
+++ code/download_gwas.sh
@@ -1,17 +1,33 @@
 
 
 dataset_names=("michalek_t1d"
-              "sakaue_t1d"
-)
+               "sakaue_t1d"
+               )
 
-urls=("ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90432001-GCST90433000/GCST90432067/harmonised/GCST90432067.h.tsv.gz"
-      "ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90018001-GCST90019000/GCST90018705/harmonised/GCST90432067.h.tsv.gz")
+urls=("https://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90432001-GCST90433000/GCST90432067/harmonised/GCST90432067.h.tsv.gz"
+      "https://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90018001-GCST90019000/GCST90018705/harmonised/34594039-GCST90018705-EFO_0001359.h.tsv.gz"
+      )
       
 for i in "${!urls[@]}"; do
     url="${urls[$i]}"
+    dataset_name="${dataset_names[$i]}"
     
-    wget -nc -O "data/gwas/${dataset_name}_full_ss.tsv.gz" "$url"
+    echo " "
+    echo "Downloading: $dataset_name"
+    echo "With url: $url"
+    echo " "
+    	
+
     
-    gunzip "data/gwas/${dataset_name}_full_ss.tsv.gz" 
+    curl -C - -s -o  "data/gwas/${dataset_name}_full_ss.tsv.gz" "$url"
     
+done
+
+for file in data/gwas/*.gz; do
+    if [ -f "$file" ]; then  # Check if the file exists
+        echo " "
+        echo "Unzipping $file..."
+        gunzip "$file"
+        #gunzip "data/gwas/${dataset_name}_full_ss.tsv.gz" 
+    fi
 done
